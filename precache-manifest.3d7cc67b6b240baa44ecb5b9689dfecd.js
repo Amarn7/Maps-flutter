5219098d64864943222e5dcb651d5cc1fc99d992
Amar Nagargoje <amargithub@gmail.com>
2020-05-02 23:30:57 +0530

    Updates

---
+++ precache-manifest.3d7cc67b6b240baa44ecb5b9689dfecd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb224ec0c8ed483eeda81b7f7ae1ece9",
+    "url": "/live-covid/index.html"
+  },
+  {
+    "revision": "8864c1f62dd8333dc8b6",
+    "url": "/live-covid/static/css/main.a9ef6350.chunk.css"
+  },
+  {
+    "revision": "990f4c24cc53b2e80824",
+    "url": "/live-covid/static/js/2.cbea47e7.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/live-covid/static/js/2.cbea47e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8864c1f62dd8333dc8b6",
+    "url": "/live-covid/static/js/main.48648b3a.chunk.js"
+  },
+  {
+    "revision": "7d050eb2a84ef529167f",
+    "url": "/live-covid/static/js/runtime-main.00297d2a.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/live-covid/static/media/image.d7265326.png"
+  }
+]);
